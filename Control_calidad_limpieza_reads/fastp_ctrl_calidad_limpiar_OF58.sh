7c2275cf960b2d338702c58c2851be44b2847347
Merce Merayo Pastor <46459548+mercemerayo@users.noreply.github.com>
2025-01-10 16:08:32 +0100

    Add files via upload

---
+++ Control_calidad_limpieza_reads/fastp_ctrl_calidad_limpiar_OF58.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Fastp sobre las lecturas Illumina de la cepa OF58 de D. subobcura
+fastp -i D1EFBACXX_4_9_1.fastq-001.gz -I D1EFBACXX_4_9_2.fastq-002.gz -o of58_r1_fp_clean.fastq.gz -O of58_r1_fp_clean.fastq.gz -h informe_fastp_of58.html 
